0a69938214474a2183c8e2dd1cda7f88c65fdf46
xbl <x_bl@sina.com>
2019-09-27 17:38:45 +0800

    project init

---
+++ scripts/mysql_create_table.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE `orders` DEFAULT CHARACTER SET = `utf8` DEFAULT COLLATE = `utf8_bin`;
+
+CREATE TABLE `order` (
+  `order_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `amount` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `total_price` double DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `product` (
+  `product_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(11) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `product` (`name`) VALUES ('Sony Mobile');
+
+select * from `product`;
