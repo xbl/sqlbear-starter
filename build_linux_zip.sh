7edff9883df9f430348bdfd6a7ca4beb40118630
xbl <x_bl@sina.com>
2019-12-20 20:47:48 +0800

    script(*): build zip

--- build_linux_zip.sh
+++ build_linux_zip.sh
@@ -7,3 +7,4 @@ sudo cp -Rf ./* $TARGET
 wget https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jre_x64_linux_hotspot_13_33.tar.gz
 tar -xf OpenJDK13U-jre_x64_linux_hotspot_13_33.tar.gz
 sudo cp -r ./jdk-13+33-jre $TARGET/jre/
+sudo zip -r $TARGET.zip $TARGET
--- build_windows_zip.sh
+++ build_windows_zip.sh
@@ -6,4 +6,4 @@ sudo mkdir -p $TARGET
 sudo cp -Rf ./* $TARGET
 wget https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jre_x64_windows_hotspot_13_33.zip
 unzip OpenJDK13U-jre_x64_windows_hotspot_13_33.zip
-sudo cp -r ./jdk-13+33-jre $TARGET/jre/
+sudo zip -r $TARGET.zip $TARGET
