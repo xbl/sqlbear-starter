149832945adf8fbe0faf08ad98fcc094875a3606
xbl <x_bl@sina.com>
2019-12-20 18:32:23 +0800

    test:

--- build_mac_zip.sh
+++ build_mac_zip.sh
@@ -7,3 +7,4 @@ sudo cp -Rf ./* $TARGET
 wget https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jre_x64_mac_hotspot_13_33.tar.gz
 tar -xf OpenJDK13U-jre_x64_mac_hotspot_13_33.tar.gz
 sudo cp -r ./jdk-13+33-jre/Contents/Home $TARGET/jre/
+zip -r /dist/sqlbear_mac_x64.zip $TARGET
